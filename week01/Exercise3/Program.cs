09e592c63e0bd0496ed75b24d76c0d0a6c3edbdf
AngelMour <angelmoure79@gmail.com>
2025-01-09 21:10:04 -0600

    Completed asigments

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        
+        int guess = -1;
+
+        while (guess != number)
+        {
+            Console.Write("What is your guess? ");
+            string input = Console.ReadLine();
+            guess = int.Parse(input);
+
+            if (guess < number)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
     }
 }
--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        
+        WelcomeMessage();
+
+        string name = GetName();
+        int number = GetNumber();
+
+        int squaredNumber = SquareNumber(number);
+
+        DisplayResults(name, number, squaredNumber);
+    }
+
+    static void WelcomeMessage()
+    {
+      
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string GetName()
+    {
+
+        Console.Write("Please enter your name? ");
+        string name = Console.ReadLine();
+
+        return name;   
+    }
+
+    static int GetNumber()
+    {
+
+        Console.Write("Please enter your favorite number? ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int squaredNumber = number * number;
+
+        return squaredNumber;
+    }
+
+    static void DisplayResults(string name, int number, int squaredNumber)
+    {
+        Console.WriteLine("Hello, " + name + "!");
+        Console.WriteLine("Your favorite number is " + number);
+        Console.WriteLine("The square of your favorite number is " + squaredNumber);
     }
 }
